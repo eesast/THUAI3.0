2877300275b41d06758b5575d1e000e8d0b7f945
Bowman <43898302+BowmanChow@users.noreply.github.com>
2019-10-09 10:15:46 +0800

    新版逻辑架构，更新消息队列 (#10)
    * 加入新的注意事项
    
    * 走路程序
    
    * first commit
    
    * 10.8 修改
    
    * 10.8 commit
    
    * 10.8 commit
    
    * 10.8 更新消息队列，常量和架构
    
    * 10.9 commit，修改了地图数据结构，改进了消息编码方式
    
    * 10.9 更新，改进了地图的数据结构，改进了信息编码方式

--- logic/Server.cs
+++ logic/Server.cs
@@ -3,196 +3,328 @@ using System.Threading;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using Constant;
+using System.Collections.Concurrent;
+using static Constant.CONSTANT;
+using System.Collections.Generic;
+
+
 namespace Server
 {
-    class Program
+    static class Program
     {
-        static void Main()
+        public static BlockingCollection<MessageToServer> messageQueue = new BlockingCollection<MessageToServer>();
+        public static List<Obj>[,] WORLD_MAP = new List<Obj>[WORLD_MAP_WIDTH, WORLD_MAP_HEIGHT];
+        private static DateTime initTime = new DateTime();
+        public static void initializeTime()
         {
-            Server server = new Server();
-            Thread[] threads = new Thread[server.player_number + 1];
-            
-            for (int i = 0; i < server.player_number; i++)
-            { threads[i] = new Thread(new ParameterizedThreadStart(server.Recv_msg)); threads[i].Start(i); }//每个玩家对应一个线程用来接收客户端发送的信息
-            threads[server.player_number] = new Thread(new ThreadStart(server.Update));//该线程负责信息更新并发送给玩家
-            threads[server.player_number].Start();
-
-            for (int i = 0; i < server.player_number + 1; i++) threads[i].Join();
+            initTime = DateTime.Now;
         }
-    }
-    public class Server
-    {
-        private readonly object myLock = new object();
-        public int player_number;
-        public byte[][] recv_buf;//用来接收玩家发送的信息
-        public string send_buf;//用来向玩家发送信息
-        public int myProt = 8885;   //端口 
-        public Socket serverSocket;
-
-        public Socket[] clientSocket;
-        Position[] position;//玩家坐标
-        MOVE_SPEED[] move_speed;//玩家移动速度
-        MOVE_DIRECTION[] MoveDirection;//移动方向：上下左右
-        long[] last_update_time;//上次刷新时间
-
-        
-        IPAddress ip;
-
-        MAP_SIZE map_size;//地图尺寸
-        bool[][] block;//用来判断碰撞的东西
-        
-        
-        public Server()
+        public static DateTime getInitTime()
         {
-            //连接到客户端
-            Console.Write("请输入游戏人数： ");
-            player_number = Convert.ToInt32(Console.ReadLine());
-            
-            string hostName = Dns.GetHostName();
-            IPHostEntry IpEntry = Dns.GetHostEntry(hostName);
-
-            for (int i = 0; i < IpEntry.AddressList.Length; i++) if (IpEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
-                { Console.WriteLine("主机地址： {0}",IpEntry.AddressList[i].ToString()); ip = IPAddress.Parse(IpEntry.AddressList[i].ToString()); }
-
-            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            serverSocket.Bind(new IPEndPoint(ip, myProt));  //绑定IP地址：端口 
-            serverSocket.Listen(player_number);    //设定最多player_number个排队连接请求 
-            Console.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());
-
-            clientSocket = new Socket[player_number];
-            //建立连接，并将游戏人数和玩家编号发给玩家
-            for (int i = 0; i < player_number; i++) { clientSocket[i] = serverSocket.Accept();send_buf = i.ToString()+" "+player_number.ToString()+" "; Sendmsg(i); }
-            //地图尺寸初始化（目前写成200*200）
-            map_size.x_width = 200;
-            map_size.y_width = 200;
-            block = new bool[map_size.x_width][];
-            for (int i = 0; i < map_size.y_width; i++) block[i] = new bool[map_size.y_width];
-            //地形初始化，目前是200*200的平地，只有撞人才会被卡住
-            map_init();
-            //各种信息初始化
-            recv_buf = new byte[player_number][];
-            for(int i=0;i<player_number;i++) recv_buf[i]=new byte[1000];
-            move_speed = new MOVE_SPEED[player_number];
-            for (int i = 0; i < player_number; i++) move_speed[i].speed = 5;
-            position = new Position[player_number];
-            position[0].x = 10; position[0].y = 10;
-            MoveDirection = new MOVE_DIRECTION[player_number];
-            last_update_time = new long[player_number];
+            return initTime;
         }
-        public void Recv_msg(object i)//从编号为i的玩家处接收信息
+        private static TimeSpan gameTime = new TimeSpan();
+        public static void refreshGameTime()
         {
-            int j = Convert.ToInt32(i);
-            while(true)
-            {
-                clientSocket[j].Receive(recv_buf[j]);
-                char[] str_msg = Encoding.ASCII.GetChars(recv_buf[j]);
-                lock (myLock)
+            gameTime = DateTime.Now - initTime;
+        }
+        public static TimeSpan getGameTime()
+        {
+            return gameTime;
+        }
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < WORLD_MAP_WIDTH; i++)
+                for (int j = 0; j < WORLD_MAP_HEIGHT; j++)
                 {
-                    if (str_msg[0] == 'u')
+                    if (
+                        (i >= 25 && i <= 75 && j >= 25 && j <= 75) ||
+                        i == 0 ||
+                        i == WORLD_MAP_WIDTH - 1 ||
+                        j == 0 ||
+                        j == WORLD_MAP_HEIGHT - 1
+                        )
                     {
-                        MoveDirection[j] = MOVE_DIRECTION.UP;
-                        move_speed[j].y_speed = move_speed[j].speed;
-                        move_speed[j].x_speed = 0;
+
+                        WORLD_MAP[i, j] = new List<Obj>();
+                        WORLD_MAP[i, j].Add(new Block(i + 0.5, j + 0.5));
                     }
-                    else if (str_msg[0] == 'd')
+                    else
                     {
-                        MoveDirection[j] = MOVE_DIRECTION.DOWN;
-                        move_speed[j].y_speed = -move_speed[j].speed;
-                        move_speed[j].x_speed = 0;
-                    }
-                    else if (str_msg[0] == 'l')
-                    {
-                        MoveDirection[j] = MOVE_DIRECTION.LEFT;
-                        move_speed[j].y_speed = 0;
-                        move_speed[j].x_speed = -move_speed[j].speed;
-                    }
-                    else if (str_msg[0] == 'r')
-                    {
-                        MoveDirection[j] = MOVE_DIRECTION.RIGHT;
-                        move_speed[j].y_speed = 0;
-                        move_speed[j].x_speed = move_speed[j].speed;
-                    }
-                    else if (str_msg[0] == 'p')
-                    {
-                        MoveDirection[j] = MOVE_DIRECTION.STOP;
+                        WORLD_MAP[i, j] = new List<Obj>();
                     }
                 }
+
+
+            Server server = new Server();
+        }
+    }
+
+
+    class Player : Character
+    {
+        private const int bufferSize = 1024;
+        private Socket socket;
+        private Thread recieveThread;
+        private byte[] recieveBuffer = new Byte[bufferSize];
+        private bool isConnected = false;
+        public bool IsConnected
+        {
+            get { return isConnected; }
+        }
+        public Player(byte id_t, double x, double y, Socket serverSocket)
+        :
+        base(id_t, x, y)
+        {
+            socket = serverSocket.Accept();
+            Console.WriteLine("{0} has connected to local.", socket.RemoteEndPoint);
+            isConnected = true;
+
+            correctPosition();
+            Program.WORLD_MAP[(uint)xyPosition.x, (uint)xyPosition.y].Add(new People(xyPosition.x, xyPosition.y));
+            recieveThread = new Thread(recieveMessage);
+            recieveThread.Start();
+
+        }
+        public bool checkXYPosition(XY_Position xyPos, uint width, uint height)
+        {
+            double halfWidth = width * 0.5;
+            double halfHeight = height * 0.5;
+
+            if (Program.WORLD_MAP[(uint)(xyPos.x + halfWidth), (uint)(xyPos.y + halfHeight)].Count > 0)
+            {
+                if (Program.WORLD_MAP[(uint)(xyPos.x + halfWidth), (uint)(xyPos.y + halfHeight)][0].GetType().Name == "Block")
+                {
+                    return false;
+                }
             }
+            if (Program.WORLD_MAP[(uint)(xyPos.x - halfWidth), (uint)(xyPos.y + halfHeight)].Count > 0)
+            {
+                if (Program.WORLD_MAP[(uint)(xyPos.x - halfWidth), (uint)(xyPos.y + halfHeight)][0].GetType().Name == "Block")
+                {
+                    return false;
+                }
+            }
+            if (Program.WORLD_MAP[(uint)(xyPos.x - halfWidth), (uint)(xyPos.y - halfHeight)].Count > 0)
+            {
+                if (Program.WORLD_MAP[(uint)(xyPos.x - halfWidth), (uint)(xyPos.y - halfHeight)][0].GetType().Name == "Block")
+                {
+                    return false;
+                }
+            }
+            if (Program.WORLD_MAP[(uint)(xyPos.x + halfWidth), (uint)(xyPos.y - halfHeight)].Count > 0)
+            {
+                if (Program.WORLD_MAP[(uint)(xyPos.x + halfWidth), (uint)(xyPos.y - halfHeight)][0].GetType().Name == "Block")
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
-        public void Sendmsg(int i)//将send_buf中的信息发送给编号为i的
+        private void correctPosition()
         {
-           clientSocket[i].Send(Encoding.ASCII.GetBytes(send_buf));
-        }
-        public void Update()//信息更新并向玩家发送
-        {
-            long t=0;
-            
+            if (xyPosition.x <= 0 || xyPosition.x >= WORLD_MAP_WIDTH - 1 || xyPosition.y <= 0 || xyPosition.y >= WORLD_MAP_HEIGHT - 1)
+            {
+                xyPosition.x = WORLD_MAP_WIDTH * 0.5;
+                xyPosition.y = WORLD_MAP_HEIGHT * 0.5;
+            }
+            if (checkXYPosition(xyPosition, 1, 1))
+                return;
+            XY_Position[] searchers = new XY_Position[4];
+            for (int i = 0; i < 4; i++)
+            {
+                searchers[i] = new XY_Position(xyPosition.x, xyPosition.y);
+            }
             while (true)
             {
-                send_buf = "";
-                //位置信息更新，含判断碰撞
-                for (int i = 0; i < player_number; i++)
+                searchers[0] = searchers[0] + new XY_Position(1, 0);
+                searchers[1] = searchers[1] + new XY_Position(0, 1);
+                searchers[2] = searchers[2] + new XY_Position(-1, 0);
+                searchers[3] = searchers[3] + new XY_Position(0, -1);
+
+                foreach (XY_Position searcher in searchers)
                 {
-                    if (last_update_time[i] >= 0 && MoveDirection[i] != MOVE_DIRECTION.STOP)
+                    if (checkXYPosition(searcher, 1, 1))
                     {
-                        t = Time();
-                        int x = (int)position[i].x;
-                        int y = (int)position[i].y;
-                        double aim_x = position[i].x + move_speed[i].x_speed * 0.001 * (t - last_update_time[i]);
-                        double aim_y = position[i].y + move_speed[i].y_speed * 0.001 * (t - last_update_time[i]);
-                        if ((block[(int)aim_x][(int)aim_y] == false ||(x == (int)aim_x && y == (int)aim_y)) && aim_x >= 0 && aim_y >= 0 && aim_x < map_size.x_width && aim_y < map_size.y_width) 
-                        {
-                            position[i].x = aim_x;
-                            position[i].y = aim_y;
-                            if (x != (int)aim_x || y != (int)aim_y) { block[x][y] = false;block[(int)aim_x][(int)aim_y] = true; }
-                        }
-                        last_update_time[i] = t;
+                        xyPosition = searcher;
+                        return;
                     }
-                    else { last_update_time[i] = Time(); }
-                    send_buf += position[i].x.ToString() + " " + position[i].y.ToString() + " ";
                 }
-                Console.Clear();
-                Console.WriteLine(send_buf);//打印一下发送给客户端的信息
-                for (int i = 0; i < player_number; i++) Sendmsg(i);
-                Thread.Sleep(30);//大约30帧的发送速率
             }
         }
-        public void map_init()//地图初始化函数
+        public override void move(DIRECTION direction)
         {
-            for (int i = 0; i < map_size.x_width; i++)
+            Program.WORLD_MAP[(uint)xyPosition.x, (uint)xyPosition.y].RemoveAll(obj => { return obj.GetType().Name == "People"; });
+            XY_Position aim = OPERATION[(uint)direction] + xyPosition;
+            if (checkXYPosition(aim, 1, 1))
             {
-                for(int j=0;j<map_size.y_width;j++)
+
+                xyPosition = aim;
+                Program.WORLD_MAP[(uint)xyPosition.x, (uint)xyPosition.y].Insert(0, new People(xyPosition.x, xyPosition.y));
+
+                sendMessage(
+                    new MessageToClient(
+                    id,
+                    xyPosition,
+                    true,
+                    new Obj(0, 0)
+                )
+                );
+            }
+            Console.WriteLine("player {0} 's position : {1}", id.ToString(), xyPosition.ToString());
+        }
+        private void recieveMessage()
+        {
+            while (true)
+            {
+                try
                 {
-                    block[i][j] = false;
+                    int bufferLength = socket.Receive(recieveBuffer);
+                    byte[] realBuffer = new Byte[bufferLength];
+                    Array.Copy(recieveBuffer, 0, realBuffer, 0, bufferLength);
+
+                    bool isSucces = false;
+                    MessageToServer msgToSvr = MessageToServer.FromBytes(realBuffer, 0, out isSucces);
+
+                    if (!isSucces)
+                        continue;
+
+                    Console.WriteLine("{0} send.", socket.RemoteEndPoint);
+                    // string[] message = str.Split(messageSpiltSeperation);
+
+                    // byte id_t = Convert.ToByte(message[0]);
+                    // // if (id_t != id)
+                    // //     continue;
+
+                    // byte type = Convert.ToByte(message[1]);
+                    // if (type < 0 || type >= (byte)COMMAND_TYPE.SIZE)
+                    //     continue;
+
+                    // byte param1 = Convert.ToByte(message[2]);
+                    // byte param2 = Convert.ToByte(message[3]);
+                    // MessageToServer msgToSvr = new MessageToServer(id, (COMMAND_TYPE)type, param1, param2);
+                    Program.messageQueue.Add(msgToSvr);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
                 }
             }
         }
-        public long Time()//计时器，返回毫秒时间
+        public void sendMessage(MessageToClient msgToClt)
         {
-            return DateTime.Now.ToFileTimeUtc() / 10000;
+            try
+            {
+                byte[] sendByte = msgToClt.ToBytes();
+                socket.Send(sendByte, sendByte.Length, 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("发送失败。");
+                Console.WriteLine(ex);
+                throw;
+            }
+
         }
     }
-    public enum MOVE_DIRECTION
+
+    class Server
     {
-        STOP = 0,
-        UP = 1,
-        DOWN = 2,
-        LEFT = 3,
-        RIGHT = 4
+        private IPAddress serverIP;//本机ip地址
+        private IPHostEntry IpEntry;
+        private IPEndPoint iPEndPoint;
+        private Socket socket;
+        private const int serverPort = 8888;
+        private const int bufferSize = 1024;
+        //private static int count = 0;//表示对话序号
+        Player[] players;
+        Thread serverThread;
+        Thread executeMessageQueueThread;
+        public Server()
+        {
+            Console.Write("请输入游戏人数： ");
+            int playerNumber = Convert.ToInt32(Console.ReadLine());
+
+            IpEntry = Dns.GetHostEntry(Dns.GetHostName());
+            for (int i = 0; i < IpEntry.AddressList.Length; i++)
+                if (IpEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine("主机地址： {0}", IpEntry.AddressList[i].ToString());
+                    serverIP = IPAddress.Parse(IpEntry.AddressList[i].ToString());
+                }
+
+            iPEndPoint = new IPEndPoint(serverIP, serverPort);
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Bind(iPEndPoint);
+                socket.Listen(playerNumber);
+                Console.WriteLine("等待连接……");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            players = new Player[playerNumber];
+            for (int i = 0; i < playerNumber; i++)
+            {
+                players[i] = new Player((byte)i, 50, 50, socket);
+            }
+
+            while (true)
+            {
+                bool isAllConnected = true;
+                for (int i = 0; i < playerNumber; i++)
+                    isAllConnected &= players[i].IsConnected;
+                if (isAllConnected)
+                    break;
+            }
+
+            serverThread = new Thread(run);
+            serverThread.Start();
+
+            Console.WriteLine("Server constructed");
+        }
+
+        public void run()
+        {
+            Program.initializeTime();
+            Console.WriteLine("Server begin to run");
+
+            executeMessageQueueThread = new Thread(executeMessageQueue);
+            executeMessageQueueThread.Start();
+
+
+
+            Console.WriteLine("Server stop running");
+        }
+        public void executeMessageQueue()
+        {
+            Console.WriteLine("Begin to execute message queue");
+            while (true)
+            {
+                Program.refreshGameTime();
+                Console.WriteLine("Time : " + Program.getGameTime().TotalSeconds.ToString() + "s");
+
+                MessageToServer msgToSvr = Program.messageQueue.Take();
+                if (msgToSvr.commandType < 0 || msgToSvr.commandType >= COMMAND_TYPE.SIZE)
+                    continue;
+
+
+                if (msgToSvr.commandType == COMMAND_TYPE.MOVE && msgToSvr.parameter1 >= 0 && msgToSvr.parameter1 < (byte)DIRECTION.SIZE)
+                {
+                    players[msgToSvr.senderID].move((DIRECTION)msgToSvr.parameter1);
+                }
+            }
+
+        }
+
     }
-    struct MOVE_SPEED
-    {
-        public float speed;
-        public float y_speed;
-        public float x_speed;
-    }
-    struct Position
-    {
-        public double x;
-        public double y;
-    }
-    struct MAP_SIZE
-    {
-        public int x_width, y_width;
-    }
+
+
 }
