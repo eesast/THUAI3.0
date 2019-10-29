68929f4cd79ed1dc2291aa07a132c22f00027dd8
zoujunhong <[982172867@qq.com]>
2019-10-29 15:51:54 +0800

    计时器用System.Threading.Timer重构

--- logic/Server Timer使用/Timer.cs
+++ logic/Server Timer使用/Timer.cs
@@ -17,16 +17,24 @@ namespace CSharp测试
             for (int i = 0; i < count; i++)
             {
                 dueTime[i] = 1;//一秒执行一次
-                callback[i] = new TimerCallback(getGameTime);//委托设定为读时间，委托需要void返回值和object参数
+                callback[i] = new TimerCallback(f);//委托设定为读时间，委托需要void返回值和object参数
             }
-            Timer myTimer=new Timer(callback,state,dueTime, count); //用上面的参数初始化一个计时器
+            Timers myTimer=new Timers(callback,state,dueTime, count); //用上面的参数初始化一个计时器
             Thread check = new Thread(myTimer.check);//检查线程，按Q可以强制计时器停止，按其他键检测到下一次回调的时间
-            myTimer.Start();//计时器启动，回调函数等待的时间也是从该语句执行的时间开始计算的
+
+            myTimer.Start_1();//计时器启动，前一个计时器的回调函数开始执行时即开始下一个计时器的计时
+            myTimer.Start_2();//计时器启动，前一个计时器的回调函数执行结束之后再开始下一个计时器的计时
+
             check.Start();//检查函数启动
 
         }
         private static DateTime initTime = new DateTime();
         public static bool run = true;
+        public static void f(object j)
+        {
+            Console.WriteLine(10);
+            Thread.Sleep(1000);
+        }
         public static void func()
         {
             initializeTime();
@@ -54,52 +62,78 @@ namespace CSharp测试
         }
         
     }
-    public class Timer
+    public class Timers
     {
         int count;//总共要执行的回调数
+
         TimerCallback[] callback;//要执行的方法
         object[] state;//第i个回调传入的参数
         double[] dueTime;//调用第i个回调函数等待的时间，默认不等待
-        double[] callbackTime;//执行第i个回调函数的时间
-        public double leftTime;//距离执行回调剩余的时间
+
+        public TimeSpan endTime;//最晚开始的计时器结束时间
+        public int flag;//正在执行的timer编号，-1表示还没开始
+        System.Threading.Timer[] timer;
         bool TimerBreak;
-        public Timer(TimerCallback[] callback_t, object[] state_t = null, double[] dueTime_t = null, int count_t = 1)
+
+        public Timers(TimerCallback[] callback_t, object[] state_t = null, double[] dueTime_t = null, int count_t = 1)
         {
             count = count_t;
             callback = callback_t;
             if (state_t != null) state = state_t;
             else state = new object[count];
             dueTime = dueTime_t;
-            callbackTime = new double[count];
             TimerBreak = false;
+            timer = new System.Threading.Timer[count];
+            flag = -1;
         }
-        public void Start()
+        public void Start_1()//调用threadFunctionType1
         {
-            Thread TimerThread = new Thread(threadFunction);
+            Thread TimerThread = new Thread(threadFunctionType1);
             TimerThread.Start();
         }
-        public void threadFunction()
+        public void Start_2()//调用threadFunctionType2
+        {
+            Thread TimerThread = new Thread(threadFunctionType2);
+            TimerThread.Start();
+        }
+        public void threadFunctionType1()//该函数使得各回调函数的开始时间间隔保持一致，而非一个执行完另一个再开始计时
         {
             for (int i = 0; i < count; i++)
             {
-                if (i == 0) callbackTime[i] = Program.gameTime.TotalSeconds + dueTime[i];
-                else callbackTime[i] = callbackTime[i - 1] + dueTime[i];
-                leftTime = callbackTime[i] - Program.gameTime.TotalSeconds;
-                while (leftTime > 0)
-                {
-                    if (TimerBreak == true) break;
-                    leftTime = callbackTime[i] - Program.gameTime.TotalSeconds;
-                    Thread.Sleep(10);
-                }
-                if (TimerBreak == true) break;
-                callback[i](state[i]);
+                if (TimerBreak == true) { break; }
+                flag++;
+                endTime = Program.gameTime + TimeSpan.FromSeconds(dueTime[i]);
+                timer[i] = new System.Threading.Timer(callback[i], state[i], TimeSpan.FromSeconds(dueTime[i]), TimeSpan.FromSeconds(0));
+                Thread.Sleep(TimeSpan.FromSeconds(dueTime[i]));
             }
         }
-        public void Quit()
+        public void threadFunctionType2(object j)//上一个计时器的回调函数执行完之后再开始下一个计时器的计时
+        {
+            void func(object k)
+            {
+                int i = Convert.ToInt32(k);
+                callback[i](state[i]);
+                if (i != count - 1)
+                {
+                    endTime = Program.gameTime + TimeSpan.FromSeconds(dueTime[(int)k + 1]);flag++;
+                    timer[i + 1] = new System.Threading.Timer(new TimerCallback(func), (int)k + 1, TimeSpan.FromSeconds(dueTime[i + 1]), TimeSpan.FromSeconds(0));
+                }
+            }
+            endTime = Program.gameTime + TimeSpan.FromSeconds(dueTime[0]);flag++;
+            timer[0] = new System.Threading.Timer(new TimerCallback(func), 0, TimeSpan.FromSeconds(dueTime[0]), TimeSpan.FromSeconds(0));
+        }
+        public void Quit()//退出计时器，且停止当前正在运行的计时器
         {
             TimerBreak = true;
+            timer[flag].Dispose();
         }
-        public void check()
+        public TimeSpan getLeftTime()//输出并返回下一个回调函数开始执行的时间
+        {
+            if (Program.gameTime < endTime) Console.WriteLine("\b \b距离下次回调还有" + (endTime - Program.gameTime) + " 当前正在进行第" + flag + "个委托");
+            else Console.WriteLine("已经全部执行");
+            return (endTime - Program.gameTime);
+        }
+        public void check()//手动检查，按q退出计时器，其他键调用getLeftTime
         {
             char key;
             while(true)
@@ -108,10 +142,11 @@ namespace CSharp测试
                 if (key == 'q' || key == 'Q')
                 {
                     Quit();
-                    Program.run = false;
+                    //Program.run = false;
+                    Console.WriteLine("\b \b定时器停止运行！");
                     break;
                 }
-                Console.WriteLine("\b \b距离下次回调还有"+leftTime+'s');
+                getLeftTime();
             }
         }
     }
