a4e39e33296554f97e8df48a08735eeb80702f49
qq1176321897 <1176321897@qq.com>
2019-10-31 21:36:25 +0800

    支持其他程序集的Message反射，清理代码 (#20)

--- communication/Proto/Message.cs
+++ communication/Proto/Message.cs
@@ -1,16 +1,32 @@
 using Google.Protobuf;
 using Google.Protobuf.Reflection;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace Communication.Proto
 {
     internal class Message : IMessage //Communication内部使用的Message
     {
+        private static readonly List<Assembly> assemblyList = new List<Assembly> //需要反射的类型程序集
+        {
+            Assembly.GetExecutingAssembly()
+        };
         public int Address; //发送者/接收者，因环境而定
         public IMessage Content; //包内容
 
         public MessageDescriptor Descriptor => null;
+
+        private static Type getType(string typename)
+        {
+            foreach (Assembly asm in assemblyList)
+            {
+                Type t = asm.GetType(typename);
+                if (t != null) return t;
+            }
+            return null;
+        }
 
         public int CalculateSize() //目前没用所以不实现
         {
@@ -22,7 +38,7 @@ namespace Communication.Proto
             BinaryReader br = new BinaryReader(stream);
             Address = br.ReadInt32();
             string PacketType = br.ReadString(); //包类型的FullName
-            Content = Activator.CreateInstance(Type.GetType(PacketType)) as IMessage;
+            Content = Activator.CreateInstance(getType(PacketType)) as IMessage;
             Content.MergeFrom(stream);
             Constants.Debug($"{PacketType} received ({Content.CalculateSize()} bytes)");
         }
@@ -31,7 +47,7 @@ namespace Communication.Proto
         {
             Address = input.ReadInt32();
             string PacketType = input.ReadString();
-            Content = Activator.CreateInstance(Type.GetType(PacketType)) as IMessage;
+            Content = Activator.CreateInstance(getType(PacketType)) as IMessage;
             Content.MergeFrom(input);
             Constants.Debug($"{PacketType} received ({Content.CalculateSize()} bytes)");
         }
