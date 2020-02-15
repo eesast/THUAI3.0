bf7dac6648538ebd5088e1b5e5fc72334efc758c
cititude <3223781449@qq.com>
2020-02-15 20:28:51 +0800

    CAPI实现Windows和Linux的基本通信模块

---
+++ CAPI/CAPI/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+// 常量和数据结构，待使用配置文件生成
+struct Player
+{
+
+};
+
+#endif
---
+++ CAPI/CAPI/player.h
@@ -0,0 +1,12 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+extern int frame ;
+extern bool GameFinished;
+void play();
+
+#endif // !PLAYER_H
+
+
+
