bbbe23d8acc064c0ff7a57316cdd78e70c71cf2b
zoujunhong <[982172867@qq.com]>
2020-03-07 14:46:44 +0800

    小改

--- communication/Proto/Constants.cs
+++ communication/Proto/Constants.cs
@@ -8,7 +8,7 @@ namespace Communication.Proto
         public delegate void DebugFunc(string DebugMessage);
         public static ushort PlayerCount = 1;
         public static ushort AgentCount = 1;
-        public static ushort ServerPort = 8888;
+        public static ushort ServerPort = 10086;
         public static readonly ushort AgentPort = 8887;
         public static readonly int MaxMessage = 3;
         public static readonly double TimeLimit = 1000;   // 在TimeLimt内agent只会转发MaxMessage条消息
