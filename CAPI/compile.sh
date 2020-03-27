728092c44c83bdea9676751e1e8bbe224e7f6056
zhangjun <zhangjun990222@qq.com>
2020-03-27 14:29:49 +0800

    CD on dev

--- CAPI/compile.sh
+++ CAPI/compile.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 cd /usr/local/CAPI 
-cat player.cpp | grep "#define DEVELOPER_ONLY"
-if [ $? -ne 0 ];then
+cat src/player.cpp | grep "#define DEVELOPER_ONLY"
+if [ $? -ne 1 ];then
 	echo "file inclusion error" >> error.txt
 	exit 1
 fi
-mkdir build && cd build && cmake .. && make >error.txt 2>&1
+mkdir build
+cd build
+cmake ..
+make >error.txt 2>&1
