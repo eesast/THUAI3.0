e6729bd0dcfb2e3544b39cbae083567b83939438
Campbell <hkp18@mails.tsinghua.edu.cn>
2020-04-14 15:43:36 +0800

    Add code role

--- CAPI/compile.sh
+++ CAPI/compile.sh
@@ -10,12 +10,12 @@ mkdir build
 cd build
 cmake ..
 make >error.txt 2>&1
-cp AI /usr/local/mnt/AI
+cp AI /usr/local/mnt/AI${THUAI_CODEROLE}
 if [ $? -ne 0]
 then
 	# compile fail
 	cp error.txt /usr/local/mnt/error.txt
-	curl -X PUT -d '{"compileInfo":@error.txt}' -H 'Content-Type: application/json' -H "Authorization: Bearer ${THUAI_COMPILE_TOKEN}" https://api.eesast.com/${THUAI_CODEID}/compile
+	curl -X PUT -d '{"compileInfo":@error.txt}' -H 'Content-Type: application/json' -H "Authorization: Bearer ${THUAI_COMPILE_TOKEN}" https://api.eesast.com/v1/codes/${THUAI_CODEID}/compile
 else
-	curl -X PUT -d '{"compileInfo":"compile success"}' -H 'Content-Type: application/json' -H "Authorization: Bearer ${THUAI_COMPILE_TOKEN}" https://api.eesast.com/${THUAI_CODEID}/compile
+	curl -X PUT -d '{"compileInfo":"compile success"}' -H 'Content-Type: application/json' -H "Authorization: Bearer ${THUAI_COMPILE_TOKEN}" https://api.eesast.com/v1/codes/${THUAI_CODEID}/compile
 fi
